f83601c9943285a20df8b32acce162b642e19d35
TiberiuPuscasu <puscasutiberiu21@stud.ase.ro>
2025-03-28 08:37:46 +0200

    Modificari

--- server.js
+++ server.js
@@ -7,7 +7,7 @@ app.use(express.json());
 app.use(express.static("public"));
 
 app.get("/", (req, res) => {
-    res.sendFile(__dirname + "/public/index.html");
+    res.sendFile(__dirname + "/public/login.html");
 });
 
 app.listen(3000, () => {
